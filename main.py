2b940a155b443aa83e15622b9dedd637c0361417
YANA SV <yanoksko12@gmail.com>
2024-01-29 20:15:20 +0200

    remade text scaling

--- main.py
+++ main.py
@@ -1,190 +1,100 @@
-from shutil import get_archive_formats
+"""
+    Main
+"""
 import random
-import os
-import glob
-import cv2
-import textwrap
-from PIL import Image, ImageDraw, ImageFont, ImageFilter
-import numpy as np
-from enum import Enum
-import functions
+import copy
+import text_functions as tf
+import layout_functions as lf
+import brochure as brr
 # -*- coding: utf-8 -*-
 
+tf.delete_file(lf.JSON_FILE)
+tf.delete_file(lf.JSON_TABLES)
+tf.delete_file(lf.JSON_TEXT_BLOCKS)
+text = lf.get_entire_text()
+br = lf.generate_brochure_params()
 
-class BlockType(Enum):
-    image = 1
-    text_and_image = 2
-    text_and_header = 3
-    text = 4
+if random.choice([True, False]):
 
+    header_bl = lf.generate_header_params(br, [0.8, 1], [0.1, 0.15], [0.07, 0.1], [0.01, 1])
+    color = tf.get_contrasting_color(br.image, header_bl)
+    font_path = tf.get_random_file_path(lf.COMMON_PATH + 'sample_fonts', '*.ttf')
+    header, main_text = tf.extract_header(text, random.randint(len(text)//200, len(text)//30))
+    lf.put_text_in_block(header, br, header_bl, font_path, color, True)
 
-def put_text_and_image_in_block(text, image, insert_image_path, x, y,
-                                block_height, block_width, font, color, draw):
+    tf.prepare_data_for_json(lf.JSON_TEXT_BLOCKS, header_bl)
+    header_height = header_bl.height + int(header_bl.height * random.uniform(0.3, 0.5))
+    segments = tf.divide_text(main_text, br.text_parts)
 
-    if block_height > block_width:
+else:
 
-        down = random.choice([True, False])
-        image_height = int(block_height * random.uniform(0.3, 0.5))
-        image_margin = int(block_height * random.uniform(0.05, 0.1))
+    segments = tf.divide_text(text, br.text_parts)
+    header_height = 0
 
-        if down:
+br.header_height = header_height
+bl = lf.generate_block_params(br)
 
-            functions.insert_image(image, insert_image_path, x, y + block_height - image_height, block_width, image_height)
-            functions.put_text_in_block(text, x, y, block_width,
-                          block_height - image_height - image_margin, font, color, draw)
-
-        else:
-
-            functions.insert_image(image, insert_image_path, x, y, block_width, image_height)
-            functions.put_text_in_block(text, x, y + image_height + image_margin, block_width,
-                          block_height - image_height - image_margin, font, color, draw)
-
-    else:
-
-        left = random.choice([True, False])
-        image_width = int(block_width * random.uniform(0.3, 0.5))
-        image_margin = int(block_width * random.uniform(0.05, 0.1))
-
-        if left:
-
-            functions.insert_image(image, insert_image_path, x, y, image_width, block_height)
-            functions.put_text_in_block(text, x + image_width + image_margin, y,
-                          block_width - image_width - image_margin, block_height, font, color, draw)
-
-        else:
-
-            functions.insert_image(image, insert_image_path, x + block_width - image_width, y, image_width, block_height)
-            functions.put_text_in_block(text, x, y, block_width - image_width - image_margin,
-                          block_height, font, color, draw)
-
-
-
-def divide_image(entire_text, index):
-
-  path = functions.get_random_file_path('sample_images', '*.jpg')
-  image = Image.open(path)
-  draw = ImageDraw.Draw(image)
-
-  width, height = image.size
-  left_margin = random.randint(10, width//20)
-  top_margin = random.randint(10, height//10)
-
-
-  if width > height:
-
-    columns = random.randint(1, 5)
-    rows = random.randint(1, 3)
-    h = int((height - 2*top_margin) * random.uniform(0.1, 0.15))
-
-  else:
-
-    columns = random.randint(1, 3)
-    rows = random.randint(1, 4)
-    h = int((height - 2*top_margin) * random.uniform(0.07, 0.1))
-
-
-  matrix, text_parts = functions.decide_on_part(columns, rows)
-  header_height = 0
-  text_pos = 0
-  header_flag = random.choice([True, False])
-
-  if header_flag:
-
-    min_length = random.randint(len(entire_text)//200, len(entire_text)//20)
-    header, main_text = functions.extract_header(entire_text, min_length)
-
-    header_width = int((width - 2*left_margin) * random.uniform(0.8, 1))
-    centre = left_margin + (width - left_margin - header_width) * random.uniform(0.01, 1)
-
-    color = functions.get_contrasting_color(image, centre, top_margin, header_width, h)
-    font_path = functions.get_random_file_path('sample_fonts', '*.ttf')
-
-    functions.put_text_in_block(header, centre, top_margin, header_width, h,
-                      font_path, color, draw)
-
-    header_height = h + (int)(height * random.uniform(0.03, 0.05))
-    segments = functions.divide_text(main_text, text_parts)
-
-  else:
-    segments = functions.divide_text(entire_text, text_parts)
-
-
-  dist_between_blocks_ver =  int((height - 2*top_margin - header_height) * random.uniform(0.02, 0.05))
-  dist_between_blocks_hor =  int((width - 2*left_margin) * random.uniform(0.02, 0.05))
-
-  block_width = int((width - 2*left_margin - (columns - 1) * dist_between_blocks_hor)/columns)
-  block_height = int((height - 2*top_margin - header_height - rows * dist_between_blocks_ver)/rows)
-
-  for row in matrix:
+for row in br.layout_matrix:
     print(row)
 
-  for j in range(columns):
-    for i in range(rows):
+text_pos = 0
+for j in range(br.columns):
+    for i in range(br.rows):
 
-        choice = matrix[i][j]
-        x = int(left_margin + j * (block_width + dist_between_blocks_hor))
-        y = int(top_margin + header_height + i * (block_height + dist_between_blocks_ver))
-        color = functions.get_contrasting_color(image, x, y, block_width, block_height)
+        choice = br.layout_matrix[i][j]
+        bl.x = int(br.left_margin + j * (bl.width + bl.hor_dist))
+        bl.y = int(br.top_margin + header_height + i * (bl.height + bl.ver_dist))
+        color = tf.get_contrasting_color(br.image, bl)
 
         if choice == 1:
 
-          path =  functions.get_random_file_path('sample_insert_images', '*.jpg')
-          functions.insert_image(image, path, x, y, block_width, block_height)
+            lf.determine_type_of_image(br, bl)
 
         elif choice == 2:
 
-          text = segments[text_pos]
-          text_pos += 1
-          font_path = functions.get_random_file_path('sample_fonts', '*.ttf')
-          image_path =  functions.get_random_file_path('sample_insert_images', '*.jpg')
-
-          put_text_and_image_in_block(text, image,
-                            image_path, x, y, block_height, block_width, font_path, color, draw)
+            text = segments[text_pos]
+            text_pos += 1
+            font_path = tf.get_random_file_path(lf.COMMON_PATH + 'sample_fonts', '*.ttf')
+            print(font_path)
+            image_path = tf.get_random_file_path(lf.COMMON_PATH + 'sample_insert_images', '*.jpg')
+            lf.put_text_and_image_in_block(text, br, bl, image_path, font_path, color)
 
         elif choice == 3:
 
-          if block_width > block_height:
-            h = int(block_height * random.uniform(0.15, 0.2))
-          else:
-            h = int(block_height * random.uniform(0.1, 0.12))
+            text = segments[text_pos] + segments[text_pos + 1]
+            text_pos += 2
+            header_bl = lf.generate_header_params_for_block(bl, [0.8, 1], [0.17, 0.2],
+                                                            [0.1, 0.12], [0.01, 1])
+            header, main_text = tf.extract_header(text,
+                                                  random.randint(len(text) // 30, len(text) // 20))
+            font_path = tf.get_random_file_path(lf.COMMON_PATH + 'sample_fonts', '*.ttf')
+            print(font_path)
+            lf.put_text_in_block(header, br, header_bl, font_path, color, True)
 
-          text = segments[text_pos] + segments[text_pos + 1]
-          text_pos += 2
-          min_header_length = random.randint(len(text)//30, len(text)//15)
+            tf.prepare_data_for_json(lf.JSON_TEXT_BLOCKS, header_bl)
 
-          header, main_text = functions.extract_header(text, min_header_length)
-          header_font_path = functions.get_random_file_path('sample_fonts', '*.ttf')
+            block_header_height = header_bl.height + int(header_bl.height * random.uniform(0.3, 0.5))
+            font_path = tf.get_random_file_path(lf.COMMON_PATH + 'sample_fonts', '*.ttf')
+            print(font_path)
+            text_bl = copy.deepcopy(bl)
+            text_bl.y += block_header_height
+            text_bl.height -= block_header_height
+            lf.put_text_in_block(main_text, br, text_bl, font_path, color, True)
 
-          functions.put_text_in_block(header, x, y, block_width, h, header_font_path, color, draw)
-          block_header_height = h + (int)(block_height * random.uniform(0.05, 0.07))
-
-          text_font_path = functions.get_random_file_path('sample_fonts', '*.ttf')
-          functions.put_text_in_block(main_text, x, y + block_header_height, block_width,
-                            block_height - block_header_height, text_font_path, color, draw)
+            tf.prepare_data_for_json(lf.JSON_TEXT_BLOCKS, text_bl)
 
         elif choice == 4:
 
-          text = segments[text_pos] + segments[text_pos + 1]
-          text_pos += 2
+            text = segments[text_pos] + segments[text_pos + 1]
+            text_pos += 2
+            font_path = tf.get_random_file_path(lf.COMMON_PATH + 'sample_fonts', '*.ttf')
+            print(font_path)
+            lf.put_text_in_block(text, br, bl, font_path, color, True)
 
-          font_path = functions.get_random_file_path('sample_fonts', '*.ttf')
-          functions.put_text_in_block(text, x, y, block_width, block_height, font_path,
-                        color, draw)
-
-
-  result_path = 'result/' + str(index) + '.jpg'
-  image.save(result_path)
+            tf.prepare_data_for_json(lf.JSON_TEXT_BLOCKS, bl)
 
 
-dataset_path = 'sample_text'
-text_files = glob.glob(os.path.join(dataset_path, '*.txt'))
-
-index = 1
-for file in text_files:
-
-  with open(file, 'r', encoding='utf-8') as f:
-      entire_text = f.read()
-  divide_image(entire_text, index)
-
-  index+=1
-
+RESULT_PATH = lf.COMMON_PATH + 'result/' + str(lf.INDEX) + '.jpg'
+br.image.save(RESULT_PATH)
+tf.delete_file('temporary_image.jpg')
+tf.delete_file('table_image.jpg')
